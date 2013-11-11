188fe611c76cd87e5824564da0620d6355a8ef31
Matias verdier <verdier.matias@gmail.com>
2013-11-11 15:54:18 -0200

    comenzando con las notificaciones de nueva version

---
+++ src/java/servlet/NotificacionNuevaVersion.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "NotificacionNuevaVersion", urlPatterns = {"/NotificacionNuevaVersion"})
+public class NotificacionNuevaVersion extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        
+        HttpSession s = request.getSession(true);
+        if (s.getAttribute("usuario") != null){
+            
+        }
+    }
+}
